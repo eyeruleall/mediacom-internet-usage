02f7902110e0cf1f2e5f814d219f6ab8657296f2
eyeruleall <eyeruleall@gmail.com>
2021-03-03 14:01:59 -0500

    Results forced to multiple lines; Fixed selectors

--- usage.js
+++ usage.js
@@ -9,10 +9,11 @@ if (!process.env.MEDIACOM_USER.includes("@mediacombb.net"))
     "Mediacom User ID must be in the form of <username>@mediacombb.net"
   );
 
-(async () => {
+const username = process.env.MEDIACOM_USER;
+const password = process.env.MEDIACOM_PASS(async () => {
   try {
     const browser = await puppeteer.launch({
-      headless: true,
+      // headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
@@ -32,8 +33,8 @@ if (!process.env.MEDIACOM_USER.includes("@mediacombb.net"))
     ]);
 
     await Promise.all([
-      await page.type('input[name="pf.pass"]', process.env.MEDIACOM_PASS),
-      await page.type('input[name="pf.username"]', process.env.MEDIACOM_USER),
+      await page.type('input[name="pf.username"]', username),
+      await page.type('input[name="pf.pass"]', password),
     ]);
 
     await Promise.all([
@@ -41,57 +42,37 @@ if (!process.env.MEDIACOM_USER.includes("@mediacombb.net"))
       page.waitForNavigation({ waitUntil: "networkidle0" }),
     ]);
 
+    const total = await page.evaluate(() =>
+      document.querySelector("internet-usage-circle").getAttribute("used")
+    );
+    const allowed = await page.evaluate(() =>
+      document.querySelector("internet-usage-circle").getAttribute("total")
+    );
+
     const percent = await page.evaluate(
       () =>
-        document
-          .querySelector(
-            "internet-usage-circle > div:nth-child(1) > div > div > div > span"
-          )
-          .innerHTML.split("%")[0]
+        document.querySelector(
+          "internet-usage-circle > div:nth-child(1) > div > div > div > span"
+        ).innerHTML
     );
-    const total = await page.evaluate(
-      () =>
-        document
-          .querySelector(
-            "internet-usage-circle > div:nth-child(1) > div > div > div > div:nth-child(2)"
-          )
-          .innerHTML.split(" GB Total")[0]
+
+    const updown = await page.evaluate(() =>
+      document.querySelector("internet-usage-circle").getAttribute("updown")
     );
-    const download = await page.evaluate(
-      () =>
-        document
-          .querySelector(
-            "internet-usage-circle > div:nth-child(1) > div > div > div > div:nth-child(3) > label:nth-child(1)"
-          )
-          .innerHTML.split(" GB Down ")[0]
-    );
-    const upload = await page.evaluate(
-      () =>
-        document
-          .querySelector(
-            "internet-usage-circle > div:nth-child(1) > div > div > div > div:nth-child(3) > label:nth-child(2)"
-          )
-          .innerHTML.split(" GB Up")[0]
-    );
-    const allowed = await page.evaluate(
-      () =>
-        document
-          .querySelector(
-            "network-usagecomponent > div > div > div.tile.white.padded > div.body > div:nth-child(2) > label.lblSmall"
-          )
-          .innerHTML.split(" GB ")[1]
-          .split("of ")[1]
-    );
+    const updownData = JSON.parse(updown);
+
+    const download = updownData.TotalDnOctetsTxt;
+    const upload = updownData.TotalUpOctetsTxt;
 
     await browser.close();
 
-    const response = {
-      percent: parseInt(percent),
-      allowed: parseInt(allowed),
-      upload: parseInt(upload),
-      download: parseInt(download),
-      total: parseInt(total),
-    };
+    const response = `{
+  percent: ${parseInt(percent)},
+  allowed: ${parseInt(allowed)},
+  upload: ${parseInt(upload)},
+  download: ${parseInt(download)},
+  total: ${parseInt(total)},
+}`;
     console.log(response);
   } catch (error) {
     console.error(error);
